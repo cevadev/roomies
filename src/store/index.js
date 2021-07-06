43567286dbaf9f0c56fdbc51da227f3dfdc61fbf
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-07-06 14:25:02 -0300

    Vue avanzado - Vuex actions/muttators

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,21 @@ export default new Vuex.Store({
       // modificamos la variable modals que contiene al modal login y le cambiamos el valor
       state.modals[name] = value;
     },
+
+    // mutator que agrega un room al objeto rooms del state
+    // le pasamos el state y un objet que contiene la nueva room y roomId
+    SET_ROOM(state, { newRoom, roomId }) {
+      // los objetos no se modifican de manera reactiva dentro de VUEJs. con Vue.set hacemos un push
+      // para actualizar los datos
+      // dentro del objeto rooms añadimos el nuevo room
+      Vue.set(state.rooms, roomId, newRoom);
+    },
+
+    // mutator que agregar un room al objeto rooms del user
+    // pasamos el state, el id del room a agregar y el id del user que se le asignará la nueva room
+    APPEND_ROOM_TO_USER(state, { roomId, userId }) {
+      Vue.set(state.users[userId].rooms, roomId, roomId);
+    },
   },
   actions: {
     /**
@@ -36,6 +51,23 @@ export default new Vuex.Store({
     TOGGLE_MODAL_STATE: ({ commit }, { name, value }) => {
       // lanzamos los datos que la mutation necesita para actualizar el state
       commit("SET_MODAL_STATE", { name, value });
+    },
+    // action que nos permite agregar un nuevo room al state
+    // commit -> lanzará los mutators
+    // room -> payload que contiene al nuevo objeto room agregado
+    CREATE_ROOM: ({ state, commit }, room) => {
+      const newRoom = room;
+      // generamos un id
+      const roomId = `room${Math.random()}`;
+      newRoom[".key"] = roomId;
+      // asignamos el id del user autenticado al nuevo room que se esta creando
+      newRoom.userId = state.authId;
+
+      // Lanzamos las mutaciones:
+      // 1. Registramos el room en el state
+      // 2. Agregamos el room al objeto user dentro del state
+      commit("SET_ROOM", { newRoom, roomId });
+      commit("APPEND_ROOM_TO_USER", { roomId, userId: newRoom.userId });
     },
   },
   getters: {
